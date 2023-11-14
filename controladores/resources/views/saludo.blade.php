06155ae67b3625841fa0db43e5bd404df96fcfd7
JoseAntonioMartinez2525 <63381930+JoseAntonioMartinez2525@users.noreply.github.com>
2023-11-14 00:57:51 +0000

    prueba

---
+++ controladores/resources/views/saludo.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html> <html> <head>
+<title>Saludo</title>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+        h1 {
+            color: blue; 
+            font-family: 'Poppins', sans-serif;      
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Hola, {{ $nombre }} .¡Bienvenido!</h1>
+</body>
+
+</html>
